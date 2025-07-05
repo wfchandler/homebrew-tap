f365f12fafe7bf4b061c74875f398b5903d765f7
wfchandler <10663805+wfchandler@users.noreply.github.com>
2025-07-05 02:24:59 +0000

    oxide-cli 0.99.0+20250704.0.1

--- Formula/oxide-cli.rb
+++ Formula/oxide-cli.rb
@@ -1,20 +1,20 @@
 class OxideCli < Formula
   desc "CLI for the Oxide rack"
-  homepage "https://github.com/oxidecomputer/oxide.rs"
-  version "0.99.0+20250704.0.0"
+  homepage "https://github.com/wfchandler/oxide.rs"
+  version "0.99.0+20250704.0.1"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/oxidecomputer/oxide.rs/releases/download/v0.99.0+20250704.0.0/oxide-cli-aarch64-apple-darwin.tar.xz"
-      sha256 "43995eea1afd88fcae9c548428f0c732445bf416d6c2acb86a79fe9972f53e58"
+      url "https://github.com/wfchandler/oxide.rs/releases/download/v0.99.0+20250704.0.1/oxide-cli-aarch64-apple-darwin.tar.xz"
+      sha256 "0f732a18baea813a404d5c7d08428c4eafd0b826382cc27936f65c810cbdbdd7"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/oxidecomputer/oxide.rs/releases/download/v0.99.0+20250704.0.0/oxide-cli-x86_64-apple-darwin.tar.xz"
-      sha256 "6b2c92fe0d96814044c0d7dd620cfb180b103b869b615eccbb14dcce0097ca19"
+      url "https://github.com/wfchandler/oxide.rs/releases/download/v0.99.0+20250704.0.1/oxide-cli-x86_64-apple-darwin.tar.xz"
+      sha256 "5e191b4669260fb7d71c0633eb91dffd7aa57a9e369f1880a7b3c5fb559a777e"
     end
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/oxidecomputer/oxide.rs/releases/download/v0.99.0+20250704.0.0/oxide-cli-x86_64-unknown-linux-gnu.tar.xz"
-    sha256 "1f72e71f46596e07f3d94c0c1dba8201a051f8dc7a837f50204ccf0198b0c6e0"
+    url "https://github.com/wfchandler/oxide.rs/releases/download/v0.99.0+20250704.0.1/oxide-cli-x86_64-unknown-linux-gnu.tar.xz"
+    sha256 "e2603c55119d56e03f40ce54cc2e0318757f491147bced81014314cdf99a4819"
   end
   license "MPL-2.0"
 
